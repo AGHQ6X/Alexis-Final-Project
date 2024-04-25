1d45c8ddb97b9148fdea7b9ae66e2f28926c318e
AGHQ6X <aghq6x@missouri.edu>
2024-04-25 16:10:50 -0500

    Added image icons to the Window.

--- final-project/src/main/java/edu/missouri/finalproject/window/Window.java
+++ final-project/src/main/java/edu/missouri/finalproject/window/Window.java
@@ -1,8 +1,14 @@
 package edu.missouri.finalproject.window;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Arrays;
+
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 import edu.missouri.finalproject.game.Game;
+import edu.missouri.finalproject.object.scoreboard.ScoreboardView;
 
 public class Window extends JFrame
 {
@@ -26,5 +32,21 @@ public class Window extends JFrame
 		
 		// Set the closing operation
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		// Try file IO
+		try
+		{
+			// Attempt to read from file
+			BufferedImage i16 = ImageIO.read(ScoreboardView.class.getResource("/Icon16.png"));
+			BufferedImage i32 = ImageIO.read(ScoreboardView.class.getResource("/Icon32.png"));
+			
+			// Set images
+			this.setIconImages(Arrays.asList(i16, i32));
+		} 
+		catch (IOException e)
+		{
+			System.err.println("Window: Image icon failed to load | " 
+					+ e.toString());
+		}
 	}
 }
